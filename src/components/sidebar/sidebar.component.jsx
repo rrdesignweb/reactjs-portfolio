4ab21745da971f4e1d821c54782841610e88cdc1
Rowan Richardson <rr.designweb@gmail.com>
2020-02-11 20:51:57 +1100

    moved inline sidebar styles into styles file

--- src/components/sidebar/sidebar.component.jsx
+++ src/components/sidebar/sidebar.component.jsx
@@ -4,64 +4,12 @@ import {
   ContactDetails,
   ProfileImg,
   SpanPrimary,
-  SpanHighlight
+  SpanHighlight,
+  MenuStyles
 } from "./sidebar.styles";
 import Me from "../../assets/me.jpg";
 
 const Sidebar = () => {
-  const MenuStyles = {
-    bmBurgerButton: {
-      position: "fixed",
-      width: "20px",
-      height: "12px",
-      left: "35px",
-      top: "35px"
-    },
-    bmBurgerBars: {
-      background: "rgba(255, 255, 255, 0.9)"
-    },
-    bmBurgerBarsHover: {
-      background: "#aaaaaa"
-    },
-    bmCrossButton: {
-      height: "24px",
-      width: "24px",
-      top: "29px",
-      left: "255px"
-    },
-    bmCross: {
-      background: "#ccc"
-    },
-    bmMenuWrap: {
-      position: "fixed",
-      height: "100%"
-    },
-    bmMenu: {
-      background: "rgba(255, 255, 255, 1)",
-      padding: "2.5em 1em 2.5em 1em",
-      fontSize: "1.45em",
-      color: "#000",
-      textDecoration: "none"
-    },
-    bmMorphShape: {
-      fill: "#cccccc"
-    },
-    bmItemList: {
-      color: "#ccc",
-      padding: "0.8em"
-    },
-    bmItem: {
-      display: "block",
-      textAlign: "left",
-      padding: "10px 0",
-      textDecoration: "none",
-      color: "#333333"
-    },
-    bmOverlay: {
-      background: "rgba(0, 0, 0, 0.5)"
-    }
-  };
-
   return (
     <Menu styles={MenuStyles}>
       <ContactDetails>
--- src/components/sidebar/sidebar.styles.jsx
+++ src/components/sidebar/sidebar.styles.jsx
@@ -2,11 +2,11 @@ import styled from "styled-components";
 import Colors from "../../constants/Colors";
 
 export const SpanPrimary = styled.span`
-    color: ${Colors.primaryGrey};
+  color: ${Colors.primaryGrey};
 `;
 
 export const SpanHighlight = styled.span`
-    color: ${Colors.highlightColor};
+  color: ${Colors.highlightColor};
 `;
 
 export const ContactDetails = styled.div`
@@ -29,5 +29,58 @@ export const Hr = styled.hr`
   margin-top: 1rem;
   margin-bottom: 1rem;
   border: 0;
-  border-top: 1px solid rgba(0,0,0,.1);
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
 `;
+
+export const MenuStyles = {
+  bmBurgerButton: {
+    position: "fixed",
+    width: "20px",
+    height: "12px",
+    left: "35px",
+    top: "35px"
+  },
+  bmBurgerBars: {
+    background: `${Colors.secondaryGrey}`
+  },
+  bmBurgerBarsHover: {
+    background: `${Colors.secondaryGrey}`
+  },
+  bmCrossButton: {
+    height: "24px",
+    width: "24px",
+    top: "29px",
+    left: "255px"
+  },
+  bmCross: {
+    background: `${Colors.solidGrey}`
+  },
+  bmMenuWrap: {
+    position: "fixed",
+    height: "100%"
+  },
+  bmMenu: {
+    background: `${Colors.white}`,
+    padding: "2.5em 1em 2.5em 1em",
+    fontSize: "1.45em",
+    color: `${Colors.black}`,
+    textDecoration: "none"
+  },
+  bmMorphShape: {
+    fill: `${Colors.solidGrey}`
+  },
+  bmItemList: {
+    color: `${Colors.solidGrey}`,
+    padding: "0.8em"
+  },
+  bmItem: {
+    display: "block",
+    textAlign: "left",
+    padding: "10px 0",
+    textDecoration: "none",
+    color: `${Colors.primaryGrey}`
+  },
+  bmOverlay: {
+    background: "rgba(0, 0, 0, 0.5)"
+  }
+};
